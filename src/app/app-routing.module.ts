1f555810e404ea00e720ebc360f9c33335c9c816
cianf4 <alessandrocianf@gmail.com>
2023-08-14 01:31:58 +0200

    Eliminate cartelle -details superflue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,10 +12,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/news/news.module').then( m => m.NewsPageModule)
   },
   {
-    path: 'news/:articleId',
-    loadChildren: () => import('./pages/news-details/news-details.module').then( m => m.NewsDetailsPageModule)
-  },
-  {
     path: 'comment/:commentId',
     loadChildren: () => import('./pages/comment/comment.module').then( m => m.CommentPageModule)
   },
@@ -24,24 +20,12 @@ const routes: Routes = [
     loadChildren: () => import('./pages/ask/ask.module').then( m => m.AskPageModule)
   },
   {
-    path: 'ask/:askId',
-    loadChildren: () => import('./pages/ask-details/ask-details.module').then( m => m.AskDetailsPageModule)
-  },
-  {
     path: 'show',
     loadChildren: () => import('./pages/show/show.module').then( m => m.ShowPageModule)
   },
   {
-    path: 'show/:showId',
-    loadChildren: () => import('./pages/show-details/show-details.module').then( m => m.ShowDetailsPageModule)
-  },
-  {
     path: 'jobs',
     loadChildren: () => import('./pages/jobs/jobs.module').then( m => m.JobsPageModule)
-  },
-  {
-    path: 'jobs/:jobId',
-    loadChildren: () => import('./pages/job-details/job-details.module').then( m => m.JobDetailsPageModule)
   },
   {
     path: 'story/:storyId',
